8fa260ccf674f152ef2ac35d4b61332a8abe591f
nonjee888 <nonjee888@gmail.com>
2023-05-02 23:05:29 +0900

    [feat] Update Tag

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -2,6 +2,6 @@ import Button from 'src/components/atom/Button';
 import Icon from 'src/components/atom/Icon';
 import Typography from 'src/components/atom/Typography';
 import TextInput from 'src/components/atom/TextInput';
+import Radio from 'src/components/atom/Radio';
 
 export { Button, TextInput, Typography, Icon, Radio };
-
