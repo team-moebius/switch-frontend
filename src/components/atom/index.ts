d4e7841390991ec52237048886137e40220f6ae2
FerrariRoma <win9592@gmail.com>
2023-05-03 21:26:37 +0900

    [feat] Create Toggle

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -3,5 +3,6 @@ import Icon from 'src/components/atom/Icon';
 import Typography from 'src/components/atom/Typography';
 import TextInput from 'src/components/atom/TextInput';
 import Radio from 'src/components/atom/Radio';
+import Toggle from 'src/components/atom/Toggle';
 
-export { Button, TextInput, Typography, Icon, Radio };
+export { Button, TextInput, Typography, Icon, Radio, Toggle };
---
+++ src/stories/Toggle.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Toggle from 'src/components/atom/Toggle';
+
+export default {
+  title: 'Toggle/basic',
+  component: Toggle,
+} as ComponentMeta<typeof Toggle>;
+
+const Template: ComponentStory<typeof Toggle> = (args) => <Toggle {...args} />;
+
+export const BasicToggle = Template.bind({});
+
+BasicToggle.storyName = 'Basic Toggle';
+BasicToggle.args = {};
