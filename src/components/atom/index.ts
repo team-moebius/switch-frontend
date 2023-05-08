cbbc1794ac46e977712f91698d9ab01217aa592a
nonjee888 <nonjee888@gmail.com>
2023-05-08 22:49:51 +0900

    [feat] Update Select

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -2,5 +2,6 @@ import Button from 'src/components/atom/Button';
 import Icon from 'src/components/atom/Icon';
 import Typography from 'src/components/atom/Typography';
 import TextInput from 'src/components/atom/TextInput';
+import Select from 'src/components/atom/Select';
 
-export { Button, Icon, TextInput, Typography };
+export { Button, Icon, TextInput, Typography, Select };
