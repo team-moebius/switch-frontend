5f56257bce5b0a241ab48340dc5a6229aa8f3c4f
Joonyoung Choi <mydream757@gmail.com>
2023-05-13 17:00:03 +0900

    Revert "[feat] Create Textarea 컴포넌트"
    This reverts commit a078e84f473ddd1353adb50d155e44683e413af7.

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -6,16 +6,5 @@ import Box from './Box';
 import FlexBox from './FlexBox';
 import Radio from 'src/components/atom/Radio';
 import ImageBox from './Image';
-import Textarea from './Textarea';
 
-export {
-  Button,
-  TextInput,
-  Typography,
-  Box,
-  FlexBox,
-  Icon,
-  Radio,
-  ImageBox,
-  Textarea,
-};
+export { Button, TextInput, Typography, Box, FlexBox, Icon, Radio, ImageBox };
