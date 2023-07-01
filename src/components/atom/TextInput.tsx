878d0a0a0014dd1bf0b8120ae294d8327776a5a0
FerrariRoma <win9592@gmail.com>
2023-07-01 22:28:41 +0900

    [feat] Update TextInput
    - onSubmitEditing을 props에 추가
    
    입력을 완료한 뒤 완료버튼을 눌렀을 때 이벤트 처리를 위해 추가했습니다.

--- src/components/atom/TextInput.tsx
+++ src/components/atom/TextInput.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { TextInput as BasicInput, StyleSheet } from 'react-native';
+import {
+  TextInput as BasicInput,
+  NativeSyntheticEvent,
+  StyleSheet,
+  TextInputEndEditingEventData,
+} from 'react-native';
 import { LengthElement } from 'src/@types/unit';
 
 interface InputProps {
   name: string;
   onChangeText: (value: string) => void;
+  onSubmitEditing?: (
+    e: NativeSyntheticEvent<TextInputEndEditingEventData>
+  ) => void;
   placeholder: string;
   value: string | undefined;
   width: LengthElement;
