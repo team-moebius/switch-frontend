71957e82466b4387217b74175b0203dbfe8e0a7a
Joonyoung Choi <mydream757@gmail.com>
2023-07-13 03:12:14 +0900

    [feat] Field Component 프로토타입

--- src/components/molecule/index.ts
+++ src/components/molecule/index.ts
@@ -1,5 +1,6 @@
 import { Checkbox } from './Checkbox';
 import { Radiobox } from './Radiobox';
 import { RadioGroup } from './RadioGroup';
+import { Field } from './Field';
 
-export { Radiobox, Checkbox, RadioGroup };
+export { Radiobox, Checkbox, RadioGroup, Field };
