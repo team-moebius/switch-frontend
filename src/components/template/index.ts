8c396047db27ecf2ce987b6656481e87acb0bd44
nonjee888 <nonjee888@gmail.com>
2023-07-30 21:08:35 +0900

    [feat] Update WithMirror, WithImage

--- src/components/template/index.ts
+++ src/components/template/index.ts
@@ -1,7 +1,7 @@
 import { ScreenWrapper } from './ScreenWrapper';
-import { WithImage } from './WithImage';
 import { WithLabel } from './WithLabel';
 import { WithInputCreator } from './WithInputCreator';
 import { WithMirror } from './WithMirror';
+import { WithImage } from './WithImage';
 
 export { WithLabel, ScreenWrapper, WithInputCreator, WithImage, WithMirror };
