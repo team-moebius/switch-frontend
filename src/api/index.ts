c33883d18cc6096feb30a40d047ccbcb7d29bb4d
Joonyoung Choi <mydream757@gmail.com>
2023-08-28 01:44:53 +0900

    [feat] UserAPI 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,5 +4,6 @@ const BookMarkApi = new API.BookmarkAPIApi();
 const ItemApi = new API.ItemAPIApi();
 const ReportAPI = new API.UserAPIApi();
 const SwitchAPI = new API.SwitchAPIApi();
+const UserApi = new API.UserAPIApi();
 
-export { BookMarkApi, ItemApi, ReportAPI, SwitchAPI };
+export { BookMarkApi, ItemApi, ReportAPI, SwitchAPI, UserApi };
