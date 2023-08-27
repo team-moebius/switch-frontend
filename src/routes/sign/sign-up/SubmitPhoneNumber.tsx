673666a4585a4f9513bf1e3573e618c2a2ea1ae7
Joonyoung Choi <mydream757@gmail.com>
2023-08-28 04:20:11 +0900

    [feat] useCommonQuery 적용 테스트 및 예시

--- src/routes/sign/sign-up/SubmitPhoneNumber.tsx
+++ src/routes/sign/sign-up/SubmitPhoneNumber.tsx
@@ -1,14 +1,20 @@
 import {
+  Bookmark,
   UserVerificationRequest,
   UserVerificationResponse,
 } from '@team-moebius/api-typescript';
 import { useState } from 'react';
 
-import { UserApi } from 'src/api';
+import { BookMarkApi, UserApi } from 'src/api';
 import { Button, Flexbox, Typography } from 'src/components/atom';
 import { Field } from 'src/components/molecule';
 import { ScreenWrapper } from 'src/components/template';
 import { useCommonMutation } from 'src/hooks/useCommomMutation';
+import { useCommonQuery } from 'src/hooks/useCommonQuery';
+
+type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any
+  ? A
+  : never;
 
 const SubmitPhoneNumber = ({ navigation }) => {
   const [state, setState] = useState<UserVerificationRequest>({ phone: '' });
@@ -26,7 +32,25 @@ const SubmitPhoneNumber = ({ navigation }) => {
     },
   });
 
-  console.debug('result:', data, isLoading, isError);
+  const {
+    data: queryData,
+    isLoading: queryLoading,
+    isSuccess,
+  } = useCommonQuery<
+    Array<Bookmark>,
+    ArgumentTypes<typeof BookMarkApi.getBookmarks>
+  >({
+    api: BookMarkApi.getBookmarks,
+    queryKey: ['bookmarks', 1234],
+    onSuccess(data) {
+      console.debug('onSuccess: ', data);
+    },
+    onError(err) {
+      console.debug(err);
+    },
+  });
+
+  console.debug('result:', queryData, queryLoading, isError, isSuccess);
   return (
     <ScreenWrapper>
       <Flexbox
