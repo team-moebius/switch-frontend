85b46a8b6f1cbc4e168fef3f6017251879586cd1
Joonyoung Choi <mydream757@gmail.com>
2023-08-31 03:32:24 +0900

    [refactor] HomeMainScreen 리팩토링 * 파일 분리 * 네이밍 수정 * 추후 재사용을 고려하여, 구성 컴포넌트를 분리함 * 현재 단계에서 구현이 불필요한 로직을 제거하고, 이후에 구현한 로직을 전달할 수 있도록 프로퍼티 구조 설계

---
+++ src/routes/root/home/HomeMainScreen/content/ItemListContent.tsx
@@ -0,0 +1,206 @@
+import React from 'react';
+import { ReactNode, useCallback, useMemo, useState } from 'react';
+import { FlatList, FlatListProps, Pressable } from 'react-native';
+import { Box, Flexbox, Icon, Select } from 'src/components/atom';
+import { ImageCard, TradingListItem } from 'src/components/molecule';
+import { StuffListItemData, STUFF_LIST_MOCK } from '../SwitchList.mock';
+
+type ViewType = 'grid' | 'list';
+
+interface ListViewProps<T>
+  extends Pick<
+    FlatListProps<T>,
+    | 'onEndReached'
+    | 'keyExtractor'
+    | 'numColumns'
+    | 'columnWrapperStyle'
+    | 'ItemSeparatorComponent'
+    | 'onEndReachedThreshold'
+    | 'renderItem'
+  > {
+  // type: ViewType;
+  data: Array<T>;
+  optionBar?: ReactNode;
+  viewKey?: string;
+}
+function ListView<T extends {}>({
+  viewKey = '',
+  data,
+  optionBar,
+  ...props
+}: ListViewProps<T>) {
+  return (
+    <Flexbox flexDirection={'column'} width={'100%'} height={'100%'}>
+      <Flexbox.Item width={'100%'}>{optionBar}</Flexbox.Item>
+      <Flexbox.Item width={'100%'} flex={1}>
+        <FlatList<T> key={viewKey} {...props} data={data} />
+      </Flexbox.Item>
+    </Flexbox>
+  );
+}
+
+const Separator = () => (
+  <Box height={1} backgroundColor={'gray'} mt={10} mb={10} />
+);
+
+interface UseFlatListArgs<T>
+  extends Pick<
+    FlatListProps<T>,
+    'onEndReached' | 'keyExtractor' | 'renderItem'
+  > {
+  type: ViewType;
+}
+
+function useFlatList<T extends {}>({
+  type,
+  onEndReached,
+  keyExtractor = (item, index) => `${index}`,
+  renderItem,
+}: UseFlatListArgs<T>): { viewKey: string } & Pick<
+  FlatListProps<T>,
+  | 'numColumns'
+  | 'columnWrapperStyle'
+  | 'ItemSeparatorComponent'
+  | 'onEndReachedThreshold'
+  | 'renderItem'
+> {
+  const props: ReturnType<typeof useFlatList<T>> = useMemo(() => {
+    switch (type) {
+      case 'grid':
+        return {
+          keyExtractor,
+          numColumns: 2,
+          onEndReachedThreshold: 0.1,
+          onEndReached,
+          renderItem,
+          columnWrapperStyle: {
+            gap: 10,
+          },
+          viewKey: `${type}`, //TODO: numColumns 를 변경할 경우, viewKey 가 동적으로 변해야함
+        };
+      case 'list':
+        return {
+          numColumns: 1,
+          onEndReached,
+          onEndReachedThreshold: 0.1,
+          renderItem,
+          ItemSeparatorComponent: Separator,
+          viewKey: `${type}`, //TODO:  numColumns 를 변경할 경우, viewKey 가 동적으로 변해야함
+        };
+    }
+  }, [type, onEndReached, keyExtractor, renderItem]);
+
+  console.debug('renderItem?', props.renderItem);
+  return props;
+}
+
+const SELECT_OPTIONS = ['무작위', '최신순', '내 위치와 가까운 순'] as const;
+type SectionOptionType = (typeof SELECT_OPTIONS)[number];
+
+const GridItem = ({ item }: { item: StuffListItemData }) => {
+  return (
+    <Flexbox.Item flex={1} width={'100%'}>
+      <ImageCard
+        title={item.name}
+        src={item.thumbnail}
+        desc={item.location}
+        width={'100%'}
+        height={150}
+        resizeMode={'cover'}
+        onClickHandler={() => {
+          window.alert('clicked');
+        }}
+      />
+    </Flexbox.Item>
+  );
+};
+
+const MemoizedGridItem = React.memo(GridItem);
+
+const ListItem = ({ item }: { item: StuffListItemData }) => {
+  return (
+    <TradingListItem
+      data={{
+        title: item.name,
+        src: item.thumbnail || '',
+        location: item.location || '',
+      }}
+      onPress={() => {
+        alert('list click');
+      }}
+      childDirection={'column'}
+      cardDirection={'row'}
+      itemJustify={'left'}
+      fontSize={'cardList'}
+      imageResizeMode={'cover'}
+    />
+  );
+};
+
+const MemoizedListItem = React.memo(ListItem);
+
+const ItemListContent = () => {
+  const [type, setType] = useState<ViewType>('grid');
+  const [sort, setSort] = useState<SectionOptionType>('무작위');
+
+  const loadMoreData = useCallback(() => {
+    console.debug('reacted end');
+  }, []);
+
+  const renderItem = useMemo(() => {
+    switch (type) {
+      case 'grid':
+        return GridItem;
+      case 'list':
+        return ListItem;
+    }
+  }, [type]);
+
+  const flatListProps = useFlatList<StuffListItemData>({
+    type,
+    onEndReached: loadMoreData,
+    renderItem,
+  });
+
+  return (
+    <ListView<StuffListItemData>
+      {...flatListProps}
+      data={STUFF_LIST_MOCK}
+      optionBar={
+        <Flexbox
+          height={'10%'}
+          width={'100%'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+        >
+          <Flexbox.Item flex={1}>
+            <Flexbox gap={5}>
+              <Pressable onPress={() => setType('grid')}>
+                <Icon
+                  name={type === 'grid' ? 'grid' : 'grid-outline'}
+                  size={20}
+                />
+              </Pressable>
+              <Pressable onPress={() => setType('list')}>
+                <Icon
+                  name={type === 'grid' ? 'list-outline' : 'list'}
+                  size={20}
+                />
+              </Pressable>
+            </Flexbox>
+          </Flexbox.Item>
+          <Flexbox.Item width={'auto'} alignSelf={'center'}>
+            <Select
+              value={sort}
+              options={['무작위', '최신순', '내 위치와 가까운 순']}
+              onPressItem={(value) => setSort(value as SectionOptionType)} //TODO: Selct 의 generic 을 수정하면, 타입 단언을 사용하지 않아도 타입 추론되도록 할 수 있을 듯
+              disabled={false}
+            />
+          </Flexbox.Item>
+        </Flexbox>
+      }
+    />
+  );
+};
+
+export { ItemListContent };
