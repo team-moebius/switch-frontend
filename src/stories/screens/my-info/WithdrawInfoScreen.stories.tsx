1222ecf041b6f871f744e2d5aa155228dca08623
FerrariRoma <win9592@gmail.com>
2023-09-21 16:26:16 +0900

    [feat] Create WithdrawInfoScreen

---
+++ src/stories/screens/my-info/WithdrawInfoScreen.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { WithdrawInfoScreen } from 'src/routes/root/my-info/WithdrawInfoScreen';
+
+export default {
+  title: 'WithdrawInfoScreen',
+  component: WithdrawInfoScreen,
+} as ComponentMeta<typeof WithdrawInfoScreen>;
+
+const Template: ComponentStory<typeof WithdrawInfoScreen> = () => (
+  <WithdrawInfoScreen />
+);
+
+export const story = Template.bind({});
+
+story.storyName = 'default';
+story.args = {};
