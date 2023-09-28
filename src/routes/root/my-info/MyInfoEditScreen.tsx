bd7c5a405c511753e6cb81ddcca9635f332b7ba1
FerrariRoma <win9592@gmail.com>
2023-09-29 01:29:51 +0900

    [feat] : Create MyInfoEditScreen

---
+++ src/routes/root/my-info/MyInfoEditScreen.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from 'react';
+import {
+  Box,
+  Flexbox,
+  Typography,
+  TextInput,
+  Separator,
+  Textarea,
+  Button,
+} from 'src/components/atom';
+import { ScreenWrapper } from 'src/components/template';
+
+const MyInfoEditScreen = () => {
+  const [name, setName] = useState('');
+  const [introduce, setIntroduce] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+
+  const nameEditHandler = (value: string) => {
+    setName(value);
+  };
+
+  const introduceEditHandler = (value: string) => {
+    setIntroduce(value);
+  };
+
+  const certifyHandler = () => {
+    alert('2차 인증');
+  };
+
+  const phoneEditHandler = () => {
+    alert('휴대폰 번호 변경');
+  };
+
+  const emailEditHandler = () => {
+    alert('이메일 변경');
+  };
+
+  const withdrawHandler = () => {
+    alert('회원 탈퇴하기');
+  };
+
+  useEffect(() => {
+    // 기존 value 할당
+    setName('집오리');
+    setIntroduce('제 꿈은 클립으로 집까지 바꾸는 거에요! :)');
+    setPhone('01012341234');
+    setEmail('');
+  }, []);
+
+  return (
+    <ScreenWrapper>
+      <Box>
+        <Separator />
+        <Flexbox alignItems='center'>
+          <Box width={80}>
+            <Typography fontSize={20}>이름</Typography>
+          </Box>
+          <TextInput
+            width={300}
+            name={'name'}
+            onChangeText={nameEditHandler}
+            placeholder={'이름을 입력해주세요.'}
+            value={name}
+          />
+        </Flexbox>
+        <Separator />
+        <Flexbox alignItems='center'>
+          <Box width={80}>
+            <Typography fontSize={20}>소개글</Typography>
+          </Box>
+          <Textarea
+            value={introduce}
+            onChangeText={introduceEditHandler}
+            placeholder='소개글을 입력해주세요.'
+          />
+        </Flexbox>
+        <Separator />
+      </Box>
+      <Flexbox.Item padding={10} flex={1}>
+        <Box mb={20}>
+          <Box width={100} mb={10}>
+            <Button type={'normal'} size={'middle'} onPress={certifyHandler}>
+              2차 인증
+            </Button>
+          </Box>
+          <Typography fontSize={15}>
+            {`실명과 휴대폰 번호를 인증합니다. 나의 프로필에 '인증 완료' 내역이 표시됩니다.`}
+          </Typography>
+        </Box>
+        <Box mb={20}>
+          <Flexbox alignItems='center'>
+            <Flexbox.Item flex={1}>
+              <Typography fontSize={20}>휴대폰 번호</Typography>
+            </Flexbox.Item>
+            <Flexbox.Item>
+              <Button
+                type={'transparent'}
+                size={'middle'}
+                onPress={phoneEditHandler}
+              >
+                {phone === '' ? '등록하기' : '변경하기'}
+              </Button>
+            </Flexbox.Item>
+          </Flexbox>
+          <Typography fontSize={15}>{phone === '' ? '-' : phone}</Typography>
+        </Box>
+        <Box>
+          <Flexbox alignItems='center'>
+            <Flexbox.Item flex={1}>
+              <Typography fontSize={20}>이메일 주소</Typography>
+            </Flexbox.Item>
+            <Flexbox.Item>
+              <Button
+                type={'transparent'}
+                size={'middle'}
+                onPress={emailEditHandler}
+              >
+                {email === '' ? '등록하기' : '변경하기'}
+              </Button>
+            </Flexbox.Item>
+          </Flexbox>
+          <Typography fontSize={15}>{email === '' ? '-' : email}</Typography>
+        </Box>
+      </Flexbox.Item>
+
+      <Button type={'transparent'} size={'middle'} onPress={withdrawHandler}>
+        회원 탈퇴하기
+      </Button>
+    </ScreenWrapper>
+  );
+};
+
+export { MyInfoEditScreen };
