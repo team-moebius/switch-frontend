51d08a6df773c7d2cb93695be0dabae63b35ff47
Joonyoung Choi <mydream757@gmail.com>
2023-10-02 11:55:19 +0900

    [update] Toggle 스타일 수정
    * false / true 일 경우의 스타일을 반대로 수정

--- src/components/molecule/Field.tsx
+++ src/components/molecule/Field.tsx
@@ -149,7 +149,7 @@ const Field = ({
             handleOnPress={() => {
               onChange({ [name]: !value });
             }}
-            backgroundColor={value ? '#E0E0E0' : '#2ECC71'}
+            backgroundColor={value === false ? '#E0E0E0' : '#2ECC71'}
           />
         );
     }
