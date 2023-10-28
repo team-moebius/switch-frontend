dda36ef3eedd4801ff2612f24ca6fa805465d213
nonjee888 <nonjee888@gmail.com>
2023-10-28 18:11:07 +0900

    [feat] Update ChatDetailScreen 진입시 마지막 메시지 뜨도록 변경

--- src/routes/root/chat/ChatDetailScreen.tsx
+++ src/routes/root/chat/ChatDetailScreen.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Box, Button, Flexbox } from 'src/components/atom';
+import React, { useRef, useState } from 'react';
+import { Button, Flexbox } from 'src/components/atom';
 import { Separator } from 'src/components/atom/Separator';
 import { HistoryListItem } from 'src/components/molecule';
 import { ScreenWrapper } from 'src/components/template';
 import { ChatInput } from './content/ChatInput';
 import ChatBubble from './content/ChatBubble';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
 type SwitchChatData = {
   id: number;
@@ -39,69 +39,196 @@ const CHAT_MOCK_DATA: SwitchChatData[] = [
     message: '이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.',
     timestamp: '2023-10-16T10:15:00',
   },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '안녕! 어떻게 지내세요?',
+    timestamp: '2023-10-16T10:00:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '안녕! 나도 잘 지내고 있어. 요즘 어떻게 지내?',
+    timestamp: '2023-10-16T10:05:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '그냥 이것저것 생각 중이에요.',
+    timestamp: '2023-10-16T10:10:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.',
+    timestamp: '2023-10-16T10:15:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '안녕! 어떻게 지내세요?',
+    timestamp: '2023-10-16T10:00:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '안녕! 나도 잘 지내고 있어. 요즘 어떻게 지내?',
+    timestamp: '2023-10-16T10:05:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '그냥 이것저것 생각 중이에요.',
+    timestamp: '2023-10-16T10:10:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.',
+    timestamp: '2023-10-16T10:15:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '안녕! 어떻게 지내세요?',
+    timestamp: '2023-10-16T10:00:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '안녕! 나도 잘 지내고 있어. 요즘 어떻게 지내?',
+    timestamp: '2023-10-16T10:05:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '그냥 이것저것 생각 중이에요.',
+    timestamp: '2023-10-16T10:10:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.',
+    timestamp: '2023-10-16T10:15:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '안녕! 어떻게 지내세요?',
+    timestamp: '2023-10-16T10:00:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '안녕! 나도 잘 지내고 있어. 요즘 어떻게 지내?',
+    timestamp: '2023-10-16T10:05:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '그냥 이것저것 생각 중이에요.',
+    timestamp: '2023-10-16T10:10:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.',
+    timestamp: '2023-10-16T10:15:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '안녕! 어떻게 지내세요?',
+    timestamp: '2023-10-16T10:00:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message: '안녕! 나도 잘 지내고 있어. 요즘 어떻게 지내?',
+    timestamp: '2023-10-16T10:05:00',
+  },
+  {
+    id: 1,
+    user: 'Alice',
+    message: '그냥 이것저것 생각 중이에요.',
+    timestamp: '2023-10-16T10:10:00',
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    message:
+      '1111이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.1111이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.1111이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.1111이해해. 생각을 정리하는 데 시간을 가져도 괜찮아.',
+    timestamp: '2023-10-16T10:15:00',
+  },
 ];
 
 const ChatDetailScreen = () => {
   const [chatText, setChatText] = useState('');
+  const scrollViewRef = useRef<ScrollView | null>(null);
+
   const onChatTextHandler = (text: string) => {
     setChatText(text);
   };
+
   return (
     <ScreenWrapper>
-      <Flexbox flexDirection={'column'} width={'100%'} height={'95%'}>
-        <Flexbox width={'100%'} height={'100%'} flexDirection={'column'}>
-          <Flexbox.Item width={'100%'}>
-            <Separator />
-          </Flexbox.Item>
-          <Flexbox.Item width={'100%'}>
-            <Flexbox
-              width={'100%'}
-              alignItems='center'
-              justifyContent='space-between'
-            >
-              <Flexbox.Item flex={1}>
-                <HistoryListItem
-                  data={{
-                    myItem: '디올 원피스',
-                    selectedItem: '커스텀 키보드',
-                  }}
-                  disabled
-                />
-              </Flexbox.Item>
-              <Flexbox.Item width={50}>
-                <Button
-                  size='small'
-                  type='normal'
-                  onPress={() => window.alert('clicked')}
-                >
-                  스위치
-                </Button>
-              </Flexbox.Item>
-            </Flexbox>
-          </Flexbox.Item>
-          <Flexbox.Item width={'100%'}>
-            <Separator />
-          </Flexbox.Item>
-          <Flexbox.Item width={'100%'}>
-            <FlatList<SwitchChatData>
+      <Flexbox width={'100%'} height={'95%'} flexDirection={'column'}>
+        <Flexbox.Item width={'100%'}>
+          <Separator />
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'}>
+          <Flexbox
+            width={'100%'}
+            alignItems='center'
+            justifyContent='space-between'
+          >
+            <Flexbox.Item flex={1}>
+              <HistoryListItem
+                data={{
+                  myItem: '디올 원피스',
+                  selectedItem: '커스텀 키보드',
+                }}
+                disabled
+              />
+            </Flexbox.Item>
+            <Flexbox.Item width={50}>
+              <Button
+                size='small'
+                type='normal'
+                onPress={() => window.alert('clicked')}
+              >
+                스위치
+              </Button>
+            </Flexbox.Item>
+          </Flexbox>
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'}>
+          <Separator />
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'} height={'90%'}>
+          <ScrollView
+            ref={scrollViewRef}
+            onContentSizeChange={() => scrollViewRef.current?.scrollToEnd()}
+          >
+            <FlatList
               data={CHAT_MOCK_DATA}
               renderItem={ChatBubble}
               keyExtractor={(item, index) => index.toString()}
               onEndReached={() => {
-                console.debug('reacted end');
+                console.debug('Reached the end');
               }}
               onEndReachedThreshold={0.1}
             />
-          </Flexbox.Item>
-        </Flexbox>
-        <Flexbox width={'100%'}>
-          <ChatInput
-            value={chatText}
-            onChangeText={onChatTextHandler}
-            placeholder={'대화 보내기'}
-            width={'85%'}
-          />
-        </Flexbox>
+          </ScrollView>
+        </Flexbox.Item>
+      </Flexbox>
+      <Flexbox width={'100%'} height={'5%'}>
+        <ChatInput
+          value={chatText}
+          onChangeText={onChatTextHandler}
+          placeholder={'대화 보내기'}
+          width={'85%'}
+        />
       </Flexbox>
     </ScreenWrapper>
   );
