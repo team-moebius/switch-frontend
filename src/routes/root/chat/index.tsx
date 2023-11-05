573670566a280cb17f1c895ec8ff6a88e92ef975
nonjee888 <nonjee888@gmail.com>
2023-11-05 14:12:12 +0900

    [feat] Create SwitchResultScreen - ScoreQuestion pr merge 후 itemJustify prop이 제 기능을 할 수 있음.

--- src/routes/root/chat/index.tsx
+++ src/routes/root/chat/index.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { ChatMainScreen } from './ChatMainScreen';
+import { SwitchResultScreen } from './SwitchResultScreen';
 
 const Stack = createStackNavigator();
 
@@ -7,6 +8,7 @@ const ChatRoute = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name={'ChatMain'} component={ChatMainScreen} />
+      <Stack.Screen name={'SwitchResult'} component={SwitchResultScreen} />
     </Stack.Navigator>
   );
 };
