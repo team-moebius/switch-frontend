543f74cc675fca3f95970e167263fdcd71f67229
nonjee888 <nonjee888@gmail.com>
2023-11-22 18:38:52 +0900

    [fix] Remove ImagePickerButton 컴포넌트

--- src/routes/root/register/SwitchDetail/Form/ImageUploader/index.tsx
+++ src/routes/root/register/SwitchDetail/Form/ImageUploader/index.tsx
@@ -1,7 +1,6 @@
-import { Flexbox, Typography } from 'src/components/atom';
-import { ScrollView } from 'react-native';
+import { Box, Flexbox, Icon, Typography } from 'src/components/atom';
+import { Pressable, ScrollView } from 'react-native';
 import { ImageItem } from './ImageItem';
-import { ImagePickerButton } from './ImagePickerButton';
 
 // Image API 명세 확인 후 구체적인 작업 필요.
 interface ImageUploaderProps {
@@ -13,9 +12,33 @@ interface ImageUploaderProps {
 const ImageUploader = ({ images, onAdd, onDeleteItem }: ImageUploaderProps) => {
   return (
     <Flexbox width={'100%'} height={'15%'} alignItems='center' gap={10}>
-      <ImagePickerButton onAdd={onAdd}>
-        <Typography fontSize={14}>{'5/5'}</Typography>
-      </ImagePickerButton>
+      <Flexbox width={'auto'}>
+        <Pressable onPress={onAdd}>
+          <Box
+            position={'relative'}
+            width={70}
+            height={70}
+            backgroundColor={'#cdcaca'}
+          >
+            <Box
+              position={'absolute'}
+              width={'auto'}
+              top={'20%'}
+              left={'33.5%'}
+            >
+              <Icon name={'camera'} size={24} />
+            </Box>
+            <Box
+              position={'absolute'}
+              width={'auto'}
+              bottom={'20%'}
+              left={'35%'}
+            >
+              <Typography fontSize={14}>{'5/5'}</Typography>
+            </Box>
+          </Box>
+        </Pressable>
+      </Flexbox>
       <Flexbox
         height={'100%'}
         width={'auto'}
