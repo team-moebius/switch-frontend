bfb585b2e2d63ebec084b56354668258b543d3e2
nonjee888 <nonjee888@gmail.com>
2023-12-26 00:29:34 +0900

    [fix] Update useExpoLocation: 버튼 누를때만 getExpoLocation 함수 호출 하도록 변경

--- src/hooks/useExpoLocation.ts
+++ src/hooks/useExpoLocation.ts
@@ -1,44 +1,41 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import * as Location from 'expo-location';
 
 const useExpoLocation = () => {
-  const [location, setLocation] = useState<Location.LocationObject | null>(
+  const [expoLocationError, setExpoLocationError] = useState<string | null>(
     null
   );
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
-  const [address, setAddress] =
-    useState<Location.LocationGeocodedAddress | null>(null);
 
-  const getLocation = async () => {
+  const [expoPostalCode, setExpoPostalCode] = useState<string | null>('');
+
+  const getExpoLocation = async () => {
     try {
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        setExpoLocationError('Permission to access location was denied');
         return;
       }
 
       const locationData = await Location.getCurrentPositionAsync({});
-      setLocation(locationData);
 
-      // Get address from coordinates
       const reverseGeocode = await Location.reverseGeocodeAsync({
         latitude: locationData.coords.latitude,
         longitude: locationData.coords.longitude,
       });
 
-      // Assuming the first result is the most relevant address
-      const firstAddress = reverseGeocode[0];
-      setAddress(firstAddress);
+      const address = reverseGeocode[0];
+      setExpoPostalCode(address?.postalCode);
     } catch (error) {
-      setErrorMsg('Error fetching location');
+      console.error('Error fetching location', error);
+      setExpoLocationError('Error fetching location');
     }
   };
 
-  useEffect(() => {
-    getLocation();
-  }, []);
-
-  return { location, errorMsg, address, getLocation };
+  return {
+    expoLocationError,
+    expoPostalCode,
+    getExpoLocation,
+  };
 };
 
 export default useExpoLocation;
