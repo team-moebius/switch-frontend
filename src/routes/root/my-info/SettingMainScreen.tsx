46d170cd95e59d5ebe8a9d9e93ab6112f7dd2015
nonjee888 <nonjee888@gmail.com>
2024-01-12 18:49:58 +0900

    [feat] Create Feedback modal

--- src/routes/root/my-info/SettingMainScreen.tsx
+++ src/routes/root/my-info/SettingMainScreen.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Flexbox, Typography } from 'src/components/atom';
+import { useCallback, useState } from 'react';
+import {
+  Box,
+  Button,
+  Flexbox,
+  Modal as FeedbackModal,
+  Typography,
+} from 'src/components/atom';
 import { ButtonProps } from 'src/components/atom/Button';
 import { ScreenWrapper } from 'src/components/template';
 
@@ -23,6 +30,17 @@ const SettingButton = ({
 };
 
 const SettingMainScreen = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleModalOpen = useCallback(() => {
+    setModalVisible((prev) => !prev);
+  }, []);
+
+  const pressFeedbackDirect = useCallback(() => {
+    setModalVisible(false);
+    navigation.navigate('Feedback');
+  }, [navigation]);
+
   return (
     <ScreenWrapper>
       <SettingButton onPress={() => navigation.navigate('Record')}>
@@ -45,10 +63,57 @@ const SettingMainScreen = ({ navigation }) => {
       <SettingButton onPress={() => navigation.navigate('Version')}>
         버전
       </SettingButton>
-      <SettingButton onPress={() => navigation.navigate('Feedback')}>
-        피드백
-      </SettingButton>
+      <SettingButton onPress={handleModalOpen}>피드백</SettingButton>
       <SettingButton onPress={() => alert('로그아웃!')}>로그아웃</SettingButton>
+      <FeedbackModal
+        visible={modalVisible}
+        onPressBack={() => setModalVisible(false)}
+        backgroundColor={'#fefefe'}
+        width={'70%'}
+        height={'35%'}
+        position={'center'}
+      >
+        <Flexbox
+          width={'100%'}
+          height={'100%'}
+          margin={'auto'}
+          gap={80}
+          flexDirection={'column'}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Flexbox.Item>
+            <Typography fontSize={14}>피드백을 어디에 남기시겠어요?</Typography>
+          </Flexbox.Item>
+          <Flexbox
+            alignItems={'center'}
+            width={'100%'}
+            flexDirection={'column'}
+            pt={20}
+            gap={10}
+          >
+            <Flexbox.Item width='90%'>
+              <Button
+                size='medium'
+                type='normal'
+                onPress={() => console.log('앱스토어')}
+              >
+                앱스토어
+              </Button>
+            </Flexbox.Item>
+            <Flexbox.Item width='90%'>
+              <Button size='medium' type='normal' onPress={pressFeedbackDirect}>
+                직접 보내기
+              </Button>
+            </Flexbox.Item>
+            <Flexbox.Item width='90%'>
+              <Button size='medium' type='cancel' onPress={handleModalOpen}>
+                취소
+              </Button>
+            </Flexbox.Item>
+          </Flexbox>
+        </Flexbox>
+      </FeedbackModal>
     </ScreenWrapper>
   );
 };
