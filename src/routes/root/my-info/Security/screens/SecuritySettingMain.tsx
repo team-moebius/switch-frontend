18a6ab126057d8debd6e16211fa68c1756501c3f
FerrariRoma <win9592@gmail.com>
2024-01-24 00:51:25 +0900

    [Update] UI layout 수정

--- src/routes/root/my-info/Security/screens/SecuritySettingMain.tsx
+++ src/routes/root/my-info/Security/screens/SecuritySettingMain.tsx
@@ -27,34 +27,40 @@ const SecuritySettingMain = ({ navigation }) => {
   return (
     <ScreenWrapper>
       <Flexbox flexDirection={'column'} gap={48}>
-        <Box>
-          <Field
-            label={<Typography fontSize={20}>앱 비밀번호 사용</Typography>}
-            name={'usePassword'}
-            fieldType={'toggle'}
-            value={usePassword}
-            onChange={({ usePassword }) => passwordChangeHandler(usePassword)}
-          />
-          <Box width={'100%'}>
-            <Typography fontSize={12}>
-              앱을 켤 때 비밀 번호를 사용합니다.
-            </Typography>
-          </Box>
-        </Box>
-        <Box>
-          <Field
-            label={<Typography fontSize={20}>생체 정보 등록</Typography>}
-            name={'useBioPassword'}
-            fieldType={'toggle'}
-            value={useBioPassword}
-            onChange={changeHandler}
-          />
-          <Box width={'100%'}>
-            <Typography fontSize={12}>
-              앱을 켤 때 비밀 번호를 사용합니다.
-            </Typography>
-          </Box>
-        </Box>
+        <Field
+          labelLayout={{ flex: 1 }}
+          childrenLayout={{ flex: 0.5 }}
+          labelAlign='center'
+          label={
+            <Flexbox flexDirection='column' gap={5}>
+              <Typography fontSize={20}>앱 비밀번호 사용</Typography>
+              <Typography fontSize={12}>
+                앱을 시작할 때 비밀 번호를 사용합니다.
+              </Typography>
+            </Flexbox>
+          }
+          name={'usePassword'}
+          fieldType={'toggle'}
+          value={usePassword}
+          onChange={({ usePassword }) => passwordChangeHandler(usePassword)}
+        />
+        <Field
+          labelLayout={{ flex: 1 }}
+          childrenLayout={{ flex: 0.5 }}
+          labelAlign='center'
+          label={
+            <Flexbox flexDirection='column' gap={5}>
+              <Typography fontSize={20}>생체 정보 등록</Typography>
+              <Typography fontSize={12}>
+                앱을 시작할 때 생체 인식을 사용합니다.
+              </Typography>
+            </Flexbox>
+          }
+          name={'useBioPassword'}
+          fieldType={'toggle'}
+          value={useBioPassword}
+          onChange={changeHandler}
+        />
       </Flexbox>
     </ScreenWrapper>
   );
