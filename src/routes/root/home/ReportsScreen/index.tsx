c68d8528b0d7f8333d8016b1247063947b6e35fe
nonjee888 <nonjee888@gmail.com>
2024-01-28 15:51:50 +0900

    [feat] Create ReportScreen

--- src/routes/root/home/ReportsScreen/index.tsx
+++ src/routes/root/home/ReportsScreen/index.tsx
@@ -1,13 +1,165 @@
-import { Flexbox, Typography } from 'src/components/atom';
+import { useState } from 'react';
+import { Pressable } from 'react-native';
+import {
+  Flexbox,
+  Icon,
+  Modal as ReportModal,
+  Modal as ConfirmModal,
+  Typography,
+  Button,
+} from 'src/components/atom';
+import { ButtonProps } from 'src/components/atom/Button';
 import { ScreenWrapper } from 'src/components/template';
 
-const ReportsScreen = () => {
+const ReportButton = ({
+  children,
+  onPress,
+}: Pick<ButtonProps, 'onPress'> & { children: string }) => {
+  return (
+    <Pressable onPress={onPress}>
+      <Flexbox
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='space-between'
+      >
+        <Typography fontSize={16}>{children}</Typography>
+        <Icon name='ios-chevron-forward' size={20} />
+      </Flexbox>
+    </Pressable>
+  );
+};
+
+const ReportsScreen = ({ navigation }) => {
+  const [reportModalVisible, setReportModalVisible] = useState(false);
+  const [confirmModalVisible, setConfirmModalVisible] = useState(false);
   return (
     <ScreenWrapper>
-      {/* write contents */}
-      <Flexbox height={'100%'} justifyContent={'center'} alignItems={'center'}>
-        <Typography fontSize={12}>Report</Typography>
+      <Flexbox
+        flexDirection={'column'}
+        height={'100%'}
+        padding={20}
+        gap={20}
+        justifyContent={'flex-start'}
+        alignItems={'flex-start'}
+      >
+        <Flexbox.Item pb={20}>
+          <Typography
+            fontSize={18}
+          >{`'${'청둥오리'}' 님을 신고합니다.`}</Typography>
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'}>
+          <ReportButton onPress={() => setReportModalVisible(true)}>
+            비속어 사용
+          </ReportButton>
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'}>
+          <ReportButton onPress={() => setReportModalVisible(true)}>
+            업자 의심
+          </ReportButton>
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'}>
+          <ReportButton onPress={() => setReportModalVisible(true)}>
+            스위치 강요
+          </ReportButton>
+        </Flexbox.Item>
+        <Flexbox.Item width={'100%'}>
+          <ReportButton onPress={() => setReportModalVisible(true)}>
+            기타 사유
+          </ReportButton>
+        </Flexbox.Item>
       </Flexbox>
+      <ReportModal
+        visible={reportModalVisible}
+        onPressBack={() => setReportModalVisible(false)}
+        backgroundColor={'#fefefe'}
+        width={'70%'}
+        height={'18%'}
+        position={'center'}
+      >
+        <Flexbox
+          width={'100%'}
+          height={'100%'}
+          margin={'auto'}
+          padding={10}
+          gap={50}
+          flexDirection={'column'}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Flexbox.Item>
+            <Typography
+              fontSize={14}
+            >{`${'청둥오리'}님을 신고 할까요?`}</Typography>
+          </Flexbox.Item>
+          <Flexbox
+            alignItems={'center'}
+            justifyContent={'center'}
+            width={'100%'}
+            gap={10}
+          >
+            <Flexbox.Item flex={1}>
+              <Button
+                size='medium'
+                type='cancel'
+                onPress={() => {
+                  setReportModalVisible(false);
+                }}
+              >
+                취소
+              </Button>
+            </Flexbox.Item>
+            <Flexbox.Item flex={1}>
+              <Button
+                size='medium'
+                type='normal'
+                onPress={() => {
+                  setConfirmModalVisible(true);
+                }}
+              >
+                확인
+              </Button>
+            </Flexbox.Item>
+          </Flexbox>
+        </Flexbox>
+        <ConfirmModal
+          visible={confirmModalVisible}
+          onPressBack={() => setConfirmModalVisible(false)}
+          backgroundColor={'#fefefe'}
+          width={'70%'}
+          height={'15%'}
+          position={'center'}
+        >
+          <Flexbox
+            width={'100%'}
+            height={'100%'}
+            margin={'auto'}
+            padding={20}
+            gap={50}
+            flexDirection={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
+          >
+            <Flexbox.Item>
+              <Typography
+                fontSize={14}
+              >{`${'청둥오리'}님을 신고했습니다.`}</Typography>
+            </Flexbox.Item>
+            <Flexbox.Item flex={1}>
+              <Button
+                size='medium'
+                type='normal'
+                onPress={() => {
+                  setReportModalVisible(false);
+                  setConfirmModalVisible(false);
+                  navigation.navigate('HomeMain');
+                }}
+              >
+                확인
+              </Button>
+            </Flexbox.Item>
+          </Flexbox>
+        </ConfirmModal>
+      </ReportModal>
     </ScreenWrapper>
   );
 };
