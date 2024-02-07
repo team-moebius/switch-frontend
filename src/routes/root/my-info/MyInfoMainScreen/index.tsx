f7f6bd7fb7823c18355fc6fd16f06e64c7c6862e
FerrariRoma <win9592@gmail.com>
2024-02-07 21:35:32 +0900

    [feat] myInfo Main data binding 및 edit 스크린에 초깃값 설정

--- src/routes/root/my-info/MyInfoMainScreen/index.tsx
+++ src/routes/root/my-info/MyInfoMainScreen/index.tsx
@@ -1,33 +1,63 @@
-import { useCallback } from 'react';
+import { useCallback, useContext } from 'react';
 
-import { Box, Button, Flexbox } from 'src/components/atom';
+import { Box, Button, Flexbox, Separator } from 'src/components/atom';
 import { UserSummary } from 'src/components/molecule';
 import { ScreenWrapper } from 'src/components/template';
 import { ListView } from 'src/components/template/ListView';
+import { ItemListCard } from './content/ItemListCard';
+
 import { useFlatList } from 'src/hooks/useFlatList';
+import { useCommonQuery } from 'src/hooks/useCommonQuery';
+import { USER_ID, expoSecureStore } from 'src/common/secureStore';
+
+import { UserApi } from 'src/api';
+import {
+  ItemResponse,
+  UserInfoItem,
+  UserInfoResponse,
+} from '@team-moebius/api-typescript';
+
 import {
   StuffListItemData,
   STUFF_LIST_MOCK,
+  MOCK_MYINFO_ITEM,
 } from '../../home/HomeMainScreen/SwitchList.mock';
-import { ItemListCard } from './content/ItemListCard';
 import { USERINFO_MOCK } from './UserInfo.mock';
+import { UserContext } from 'src/context/user';
 
 const MyInfoMainScreen = ({ navigation }) => {
+  const { userId } = useContext(UserContext);
+
+  const { data, isLoading, isSuccess } = useCommonQuery<
+    UserInfoResponse,
+    Parameters<typeof UserApi.getUserInfo>
+  >({
+    api: UserApi.getUserInfo,
+    queryKey: ['myInfoMain_userApi_getUserInfo', userId],
+    onSuccess(data) {
+      console.debug('\n\n✅ myInfoMain_userApi_getUserInfo ✅\n', data);
+    },
+    onError(err) {
+      console.debug('\n\n🚨 myInfoMain_userApi_getUserInfo 🚨\n', err);
+    },
+  });
+
   const loadMoreData = useCallback(() => {
     console.debug('reacted end');
   }, []);
 
-  const renderItem = useCallback(({ item }: { item: StuffListItemData }) => {
+  const renderItem = useCallback(({ item }: { item: ItemResponse }) => {
     return (
       <ItemListCard
-        title={item.name}
+        title={item.name ?? ''}
         count={item.waitingCount}
-        imageSrc={item.thumbnail}
+        // imageSrc={item.thumbnail}
+        imageSrc={item.images ? item.images[0] : ''}
       />
     );
   }, []);
 
-  const flatListProps = useFlatList<StuffListItemData>({
+  const flatListProps = useFlatList<ItemResponse>({
     type: 'grid',
     onEndReached: loadMoreData,
     renderItem,
@@ -38,11 +68,11 @@ const MyInfoMainScreen = ({ navigation }) => {
       <Box>
         <UserSummary
           data={{
-            user: USERINFO_MOCK.userName,
-            verified: USERINFO_MOCK.verified,
-            countSwitch: USERINFO_MOCK.switchCount,
-            userRate: USERINFO_MOCK.userRate,
-            bio: USERINFO_MOCK.introduce,
+            user: data?.nickname ?? (userId as string),
+            verified: !!data?.phone,
+            countSwitch: data?.switchCount ?? 0,
+            userRate: data?.score ?? 0,
+            bio: data?.introduction,
           }}
         />
         <Flexbox justifyContent='center' alignItems='center' mt={10} mb={10}>
@@ -51,7 +81,7 @@ const MyInfoMainScreen = ({ navigation }) => {
               type={'normal'}
               size={'medium'}
               onPress={function (): void {
-                navigation.navigate('MyInfoEdit');
+                navigation.navigate('MyInfoEdit', { userInfo: data });
               }}
             >
               내 정보 편집하기
@@ -59,12 +89,9 @@ const MyInfoMainScreen = ({ navigation }) => {
           </Box>
         </Flexbox>
       </Box>
-      <Box height={1} width={'100%'} backgroundColor='#000000' />
+      <Separator />
       <Flexbox height={'100%'}>
-        <ListView<StuffListItemData>
-          {...flatListProps}
-          data={STUFF_LIST_MOCK}
-        />
+        <ListView<ItemResponse> {...flatListProps} data={STUFF_LIST_MOCK} />
       </Flexbox>
     </ScreenWrapper>
   );
