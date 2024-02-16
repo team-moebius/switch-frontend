d46a2124554162f06ba32689d81d29b919566f4c
FerrariRoma <win9592@gmail.com>
2024-02-16 17:36:12 +0900

    [bugfix] 수정된 api에 맞게 에러나는 부분 수정

--- src/utils/getPageableContent.ts
+++ src/utils/getPageableContent.ts
@@ -1,10 +1,6 @@
 import { InfiniteData } from 'react-query';
-import { SliceItem as CommonSlice } from '@team-moebius/api-typescript/dist/src/model/slice-item';
+import { SliceItem } from '@team-moebius/api-typescript/dist/src/model/slice-item';
 
-interface SliceItem<T> extends Omit<CommonSlice, 'content'> {
-  content: Array<T>;
-}
-
-export const getPageableContent = (
-  data: InfiniteData<SliceItem<any>> | undefined
+export const getPageableContent = <T extends SliceItem>(
+  data: InfiniteData<T> | undefined
 ) => data?.pages.map((page) => (page.content ? page.content : [])).flat() ?? [];
