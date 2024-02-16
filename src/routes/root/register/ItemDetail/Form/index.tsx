33b2b1ceeeb696413ff42893217e82c1e1382755
FerrariRoma <win9592@gmail.com>
2024-02-16 12:04:00 +0900

    [chore] PR 코멘트 반영

--- src/routes/root/register/ItemDetail/Form/index.tsx
+++ src/routes/root/register/ItemDetail/Form/index.tsx
@@ -21,9 +21,6 @@ import { ScreenWrapper } from 'src/components/template';
 export const DETAILS = 'details';
 export const SAFETY = 'safety';
 
-const DETAILS = 'details';
-const SAFETY = 'safety';
-
 interface SwitchDetailFormProps {
   initialData?: SwitchDetailData;
   onSubmit?: (data: SwitchDetailData) => void;
