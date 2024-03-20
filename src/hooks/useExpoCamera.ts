303a173e32de04480518100ae761a39351063916
FerrariRoma <win9592@gmail.com>
2024-03-21 02:22:02 +0900

    [feat] imagePicker에서 권한 설정 시 기본 제공된 훅 적용

--- src/hooks/useExpoCamera.ts
+++ src/hooks/useExpoCamera.ts
@@ -1,13 +1,35 @@
 import { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import { Alert } from 'react-native';
 
 const useExpoCamera = () => {
   const [photoUri, setPhotoUri] = useState<string | null>(null);
 
-  const openCamera = async () => {
-    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+  const [cameraPermissionInfo, requestPermission] =
+    ImagePicker.useCameraPermissions();
 
-    if (permissionResult.status !== 'granted') {
+  const verifyPermissions = async () => {
+    if (
+      cameraPermissionInfo?.status === ImagePicker.PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInfo?.status === ImagePicker.PermissionStatus.DENIED) {
+      Alert.alert('권한이 없습니다.', '카메라 접근 권한이 필요합니다.');
+
+      return false;
+    }
+
+    return true;
+  };
+
+  const openCamera = async () => {
+    const permissionResult = await verifyPermissions();
+
+    if (!permissionResult) {
       return {
         error: 'denied',
       };
@@ -17,6 +39,7 @@ const useExpoCamera = () => {
       quality: 0.7,
       allowsEditing: false,
       aspect: [16, 9],
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
     });
 
     if (cameraImage.canceled || !cameraImage.assets.length) {
--- src/hooks/useExpoImagePicker.ts
+++ src/hooks/useExpoImagePicker.ts
@@ -1,24 +1,45 @@
 import { useState } from 'react';
-import { Platform } from 'react-native';
+import { Alert, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { getFormat } from 'src/utils/getFormat';
 
 const useExpoImagePicker = () => {
+  const [libraryPermissionInfo, requestPermission] =
+    ImagePicker.useMediaLibraryPermissions();
+
+  const verifyPermissions = async () => {
+    if (
+      libraryPermissionInfo?.status ===
+      ImagePicker.PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (libraryPermissionInfo?.status === ImagePicker.PermissionStatus.DENIED) {
+      Alert.alert('권한이 없습니다.', '사진 접근 권한이 필요합니다.');
+
+      return false;
+    }
+
+    return true;
+  };
+
   const [selectedImages, setSelectedImages] = useState<string[]>([]);
 
   const pickImage = async (extensions?: string[]) => {
     const ios = Platform.OS === 'ios';
-    const permissionCameraRollResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const permissionCameraRollResult = await verifyPermissions();
 
-    if (permissionCameraRollResult.status !== 'granted') {
+    if (!permissionCameraRollResult) {
       return {
         error: 'denied',
       };
     }
 
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsMultipleSelection: true,
       aspect: [4, 3],
       quality: 1,
