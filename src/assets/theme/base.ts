4745cf8c74df01a0eb73510fb045dfda89ae9f1d
FerrariRoma <win9592@gmail.com>
2024-10-26 18:05:34 +0900

    [feat] base와 palettes 파일명 및 변수명 수정
    - 기존 base가 palettes로, 기존 palettes가 base로 가는 것이 더 적합하다고 판단하여 수정했습니다.
    - palette에 실제로 수치에 따라 색상을 다르게 설정했습니다.

---
+++ src/assets/theme/base.ts
@@ -0,0 +1,31 @@
+import PALETTE from './colors/palettes';
+
+const SEMANTIC_COLORS = {
+  success: PALETTE.green['200'],
+  warning: PALETTE.yellow['100'],
+  error: PALETTE.red['200'],
+  info: PALETTE.blue['200'],
+} as const;
+
+const SYSTEM_COLORS = {
+  primary: PALETTE.blue,
+  secondary: PALETTE.green,
+  neutral: {
+    white: PALETTE.white['100'],
+    gray: PALETTE.gray['100'],
+    black: PALETTE.black['100'],
+  },
+} as const;
+
+const COMPONENT_COLORS = {
+  text: SYSTEM_COLORS.neutral.black,
+  container_background: SYSTEM_COLORS.neutral.white,
+} as const;
+
+const COLORS = {
+  ...SEMANTIC_COLORS,
+  ...SYSTEM_COLORS,
+  ...COMPONENT_COLORS,
+} as const;
+
+export default COLORS;
