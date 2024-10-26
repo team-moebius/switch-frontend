ee8c2e23686eb5ded7675e81939ff07e8184d756
FerrariRoma <win9592@gmail.com>
2024-10-26 19:10:17 +0900

    [feat] colors, font-size 적용 및 getRateString 함수 제거
    - 프론트 단에서 점수를 계산해 줄 필요가 없는 관계로 제거했습니다.

--- src/components/molecule/UserSummary.tsx
+++ src/components/molecule/UserSummary.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Flexbox, Tag, Typography } from '../atom';
-import COLORS from 'src/assets/theme/base';
+import { COLORS, FONT_SIZE } from 'src/assets/theme/base';
 
 type UserSummaryData = {
   nickname: string;
@@ -13,23 +13,16 @@ interface UserSummaryProps {
   data: UserSummaryData;
 }
 
-const VERIFIED_COLOR = '#449afc';
-const UNVERIFIED_COLOR = '#ed692c';
-
-const getRateString = (value: number, total: number): `${number}/${number}` => {
-  return `${Number((value * total).toFixed(2))}/${total}`;
-};
-
 const UserSummary = ({ data }: UserSummaryProps) => {
   const { nickname, verified, switchCount, score, introduction } = data;
-  const tagColor = verified ? VERIFIED_COLOR : UNVERIFIED_COLOR;
+  const tagColor = verified ? COLORS.success : COLORS.error;
 
   return (
     <Box>
       <Flexbox>
         <Flexbox.Item flex={1}>
           <Flexbox flexDirection={'column'} justifyContent='center' gap={5}>
-            <Typography fontSize={20}>{nickname}</Typography>
+            <Typography fontSize={FONT_SIZE.header}>{nickname}</Typography>
             <Tag color={COLORS.neutral.white} backgroundColor={tagColor}>
               {verified ? '인증완료' : '미인증 유저'}
             </Tag>
@@ -37,21 +30,20 @@ const UserSummary = ({ data }: UserSummaryProps) => {
         </Flexbox.Item>
         <Flexbox.Item alignSelf='center'>
           <Flexbox alignItems='center' justifyContent='center'>
-            <Typography fontSize={13}>
+            <Typography fontSize={FONT_SIZE.smaller}>
               {`스위치 횟수: ${switchCount} 점`}
             </Typography>
           </Flexbox>
           <Flexbox alignItems='center' justifyContent='center' mt={10}>
-            <Typography fontSize={13}>{`스위처 점수: ${getRateString(
-              score,
-              5
-            )}`}</Typography>
+            <Typography
+              fontSize={FONT_SIZE.smaller}
+            >{`스위처 점수: ${score} / 5`}</Typography>
           </Flexbox>
         </Flexbox.Item>
       </Flexbox>
       {introduction && (
         <Box mt={10}>
-          <Typography fontSize={13}>{introduction}</Typography>
+          <Typography fontSize={FONT_SIZE.smaller}>{introduction}</Typography>
         </Box>
       )}
     </Box>
