5a69e61622bdd025d7a91a74f84bf7b6e66bbcfc
FerrariRoma <win9592@gmail.com>
2024-10-28 15:59:08 +0900

    [feat] UserSummary UI 수정
    - 불필요한 CSS 걷어냈습니다.
    - nickname 가운데 정렬했습니다.
    - nickname이나 introduction이 길 경우를 대비해 ellipsizeMode를 설정했습니다.

--- src/components/molecule/UserSummary.tsx
+++ src/components/molecule/UserSummary.tsx
@@ -1,49 +1,57 @@
-import React from 'react';
 import { Box, Flexbox, Tag, Typography } from '../atom';
 import { COLORS, FONT_SIZE } from 'src/assets/theme/base';
 
-type UserSummaryData = {
+interface UserSummaryData {
   nickname: string;
   verified: boolean;
   switchCount: number;
   score: number;
   introduction?: string;
-};
+}
 interface UserSummaryProps {
   data: UserSummaryData;
 }
 
 const UserSummary = ({ data }: UserSummaryProps) => {
   const { nickname, verified, switchCount, score, introduction } = data;
-  const tagColor = verified ? COLORS.success : COLORS.error;
+  const tagColor = verified ? COLORS.info : COLORS.error;
 
   return (
     <Box>
-      <Flexbox>
+      <Flexbox justifyContent='space-between'>
         <Flexbox.Item flex={1}>
-          <Flexbox flexDirection={'column'} justifyContent='center' gap={5}>
-            <Typography fontSize={FONT_SIZE.header}>{nickname}</Typography>
+          <Flexbox flexDirection='column' alignItems='center' rowGap={5}>
+            <Typography
+              fontSize={FONT_SIZE.header}
+              ellipsizeMode='tail'
+              numberOfLines={1}
+            >
+              {nickname}
+            </Typography>
+
             <Tag color={COLORS.neutral.white} backgroundColor={tagColor}>
               {verified ? '인증완료' : '미인증 유저'}
             </Tag>
           </Flexbox>
         </Flexbox.Item>
-        <Flexbox.Item alignSelf='center'>
-          <Flexbox alignItems='center' justifyContent='center'>
-            <Typography fontSize={FONT_SIZE.smaller}>
-              {`스위치 횟수: ${switchCount} 점`}
-            </Typography>
-          </Flexbox>
-          <Flexbox alignItems='center' justifyContent='center' mt={10}>
-            <Typography
-              fontSize={FONT_SIZE.smaller}
-            >{`스위처 점수: ${score} / 5`}</Typography>
-          </Flexbox>
-        </Flexbox.Item>
+        <Flexbox flexDirection='column' rowGap={8}>
+          <Typography fontSize={FONT_SIZE.normal}>
+            {`스위치 횟수: ${switchCount} 점`}
+          </Typography>
+          <Typography
+            fontSize={FONT_SIZE.normal}
+          >{`스위처 점수: ${score} / 5`}</Typography>
+        </Flexbox>
       </Flexbox>
       {introduction && (
         <Box mt={10}>
-          <Typography fontSize={FONT_SIZE.smaller}>{introduction}</Typography>
+          <Typography
+            fontSize={FONT_SIZE.smaller}
+            ellipsizeMode='tail'
+            numberOfLines={2}
+          >
+            {introduction}
+          </Typography>
         </Box>
       )}
     </Box>
