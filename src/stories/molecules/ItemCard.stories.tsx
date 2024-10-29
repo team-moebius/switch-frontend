561f41d345f34ee34713599d59e707a28683f105
FerrariRoma <win9592@gmail.com>
2024-10-29 15:03:10 +0900

    [feat] 프로퍼티 명 백엔드와 일치시키기
    - 프로퍼티 명 수정했습니다.

--- src/stories/molecules/ItemCard.stories.tsx
+++ src/stories/molecules/ItemCard.stories.tsx
@@ -16,11 +16,12 @@ export const story = Template.bind({});
 story.storyName = 'default';
 story.args = {
   data: {
-    title: '커스텀 키보드',
+    name: '커스텀 키보드',
     date: '2023/03/08',
-    desc: '신입 개발자 시절 일에 대한 열정을 불태워줄 수 있도록 도와줬던 키보드입니다. 이걸로 업무봤을 때 좀 더 잘 되는 것 같았어요.',
+    description:
+      '신입 개발자 시절 일에 대한 열정을 불태워줄 수 있도록 도와줬던 키보드입니다. 이걸로 업무봤을 때 좀 더 잘 되는 것 같았어요.',
     preferredCategories: ['여성 신발', '잡화'],
-    location: '서울 천왕동',
+    preferredLocations: ['서울 천왕동'],
     liked: false,
   },
   onLikeHandler: () => {
