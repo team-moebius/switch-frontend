7440fea3ce6030945af7163062d8331fe6cb3428
FerrariRoma <win9592@gmail.com>
2024-11-08 21:14:12 +0900

    [feat] CATEGORY 추가 및 타입 정의

--- src/routes/root/register/RegisterFormScreen/index.tsx
+++ src/routes/root/register/RegisterFormScreen/index.tsx
@@ -55,6 +55,27 @@ interface RegisterFormProps {
   initialData?: ItemResponse;
   getAddress?: string;
 }
+const REGISTER_CATEGORY = [
+  '수입명품',
+  '패션의류',
+  '패션잡화',
+  '뷰티',
+  '출산 / 유아동',
+  '모바일 / 태블릿',
+  '가전제품',
+  '노트북 / PC',
+  '카메라 / 캠코더',
+  '가구 / 인테리어',
+  '리빙 / 생활',
+  '게임',
+  '반려동물 / 취미',
+  '도서 / 음반 / 문구',
+  '티켓 / 쿠폰',
+  '스포츠',
+  '레저 / 여행',
+  '공구 / 산업용품',
+] as const;
+type RegisterCategory = (typeof REGISTER_CATEGORY)[number];
 
 const defaultForm = {
   name: '',
