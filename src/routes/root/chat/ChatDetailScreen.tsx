89e92decd1f543928941e67e7357c8e59c8f39e7
FerrariRoma <win9592@gmail.com>
2025-01-12 05:45:55 +0900

    [feat] ChatDetailScreen UI 수정

--- src/routes/root/chat/ChatDetailScreen.tsx
+++ src/routes/root/chat/ChatDetailScreen.tsx
@@ -1,17 +1,27 @@
-import { useContext, useEffect, useRef, useState } from 'react';
-import { Box, Button, Flexbox } from 'src/components/atom';
-import { Separator } from 'src/components/atom/Separator';
+import { useContext, useEffect, useState } from 'react';
+import {
+  InputAccessoryView,
+  Platform,
+  FlatList,
+  KeyboardAvoidingView,
+} from 'react-native';
+
+import { Box, Button, Flexbox, Separator } from 'src/components/atom';
 import { HistoryListItem, PressableIcon } from 'src/components/molecule';
-import { ScreenWrapper } from 'src/components/template';
+
 import { ChatInput } from './content/ChatInput';
 import ChatBubble from './content/ChatBubble';
-import { FlatList } from 'react-native-gesture-handler';
 import { SwitchCompleteModal } from './content/modals/SwitchCompleteModal';
-import { StackScreenProps } from '@react-navigation/stack';
+
 import { ChatRouteParamList } from '.';
-import { COLORS } from 'src/assets/theme/base';
-import { UserContext } from 'src/context/user';
 import { SubCallbackProps, useSocket } from 'src/context/socket';
+
+import { UserContext } from 'src/context/user';
+import { ThemeContext } from 'src/context/theme';
+
+import { StackScreenProps } from '@react-navigation/stack';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { PADDING } from 'src/assets/theme/base';
 
 type SwitchChatData = {
   id: number;
@@ -171,8 +181,10 @@ const CHAT_MOCK_DATA: SwitchChatData[] = [
 const ChatDetailScreen = ({
   navigation,
 }: StackScreenProps<ChatRouteParamList, 'ChatDetail'>) => {
+  const { color } = useContext(ThemeContext);
   const { userId } = useContext(UserContext);
   const { send, subscribe, unsubscribe, isConnected } = useSocket();
+  const bottomHeight = useBottomTabBarHeight();
 
   const [chatText, setChatText] = useState('');
   const [completeModalVisible, setCompleteModalVisible] = useState(false);
@@ -193,6 +205,7 @@ const ChatDetailScreen = ({
   };
 
   const onSubMessage = (message: SubCallbackProps) => {
+    console.log(message);
     setMessageData((prev) => [message, ...prev]);
   };
 
@@ -203,77 +216,71 @@ const ChatDetailScreen = ({
   }, [isConnected]);
 
   return (
-    <ScreenWrapper>
-      <Flexbox width={'100%'} height={'94%'} flexDirection={'column'}>
-        <Flexbox.Item width={'100%'} height={'auto'}>
-          <Flexbox.Item width={'100%'}>
-            <Separator />
-          </Flexbox.Item>
-          <Flexbox.Item width={'100%'}>
-            <Flexbox
-              width={'100%'}
-              alignItems='center'
-              justifyContent='space-between'
-            >
-              <Flexbox.Item flex={1}>
-                <HistoryListItem
-                  data={{
-                    myItem: '디올 원피스',
-                    selectedItem: '커스텀 키보드',
-                  }}
-                  disabled
-                />
-              </Flexbox.Item>
-              <Flexbox.Item width={50}>
-                <Button
-                  size='small'
-                  type='normal'
-                  onPress={() => setCompleteModalVisible(true)}
-                >
-                  스위치
-                </Button>
-              </Flexbox.Item>
-            </Flexbox>
-          </Flexbox.Item>
-          <Flexbox.Item width={'100%'}>
-            <Separator />
-          </Flexbox.Item>
-        </Flexbox.Item>
-        <Flexbox.Item width={'100%'} flex={1}>
-          <FlatList
-            data={messageData}
-            renderItem={ChatBubble}
-            onEndReachedThreshold={0.1}
-            onEndReached={() => {
-              console.debug('Reached the end');
-                const copy = prev.slice();
-                const reversed = [...CHAT_MOCK_DATA].reverse();
-            }}
-            ref={scrollViewRef}
-            inverted
-          />
-        </Flexbox.Item>
-      </Flexbox>
+    <Flexbox.Item flex={1} backgroundColor={color.container_background}>
+      <Separator />
       <Flexbox
         width={'100%'}
-        height={'auto'}
-        mt={10}
-        backgroundColor={COLORS.container_background}
+        alignItems='center'
+        justifyContent='space-between'
+        backgroundColor={color.container_background}
+        pl={PADDING.wrapper.horizontal}
+        pr={PADDING.wrapper.horizontal}
       >
-        <ChatInput
-          value={chatText}
-          onChangeText={onChatTextHandler}
-          placeholder={'대화 보내기'}
-          width={'80%'}
-          right={
-            <PressableIcon
-              name={'paper-plane-outline'}
-              size={24}
-              onPress={onSendChatMessage}
-            />
-          }
-        />
+        <Flexbox.Item flex={1}>
+          <HistoryListItem
+            data={{
+              myItem: '디올 원피스',
+              selectedItem: '커스텀 키보드',
+            }}
+            disabled
+          />
+        </Flexbox.Item>
+        <Flexbox.Item width={50}>
+          <Button
+            size='small'
+            type='normal'
+            onPress={() => setCompleteModalVisible(true)}
+          >
+            스위치
+          </Button>
+        </Flexbox.Item>
       </Flexbox>
+      <Separator />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={95}
+        style={{ flex: 1 }}
+      >
+        <FlatList
+          data={messageData}
+          renderItem={({ item }) => (
+            <ChatBubble
+              isMine={(userId as string) === `${item.senderId}`}
+              content={item.content}
+            />
+          )}
+          onEndReachedThreshold={0.1}
+          onEndReached={() => {
+            console.debug('Reached the end');
+          }}
+          inverted
+        />
+        <Box>
+          <ChatInput
+            value={chatText}
+            backgroundColor={color.container_background}
+            onChangeText={onChatTextHandler}
+            placeholder={'대화 보내기'}
+            right={
+              <PressableIcon
+                name={'paper-plane-outline'}
+                size={24}
+                onPress={onSendChatMessage}
+              />
+            }
+          />
+        </Box>
+      </KeyboardAvoidingView>
       <SwitchCompleteModal
         visible={completeModalVisible}
         onPressBack={() => setCompleteModalVisible(false)}
@@ -282,7 +289,7 @@ const ChatDetailScreen = ({
           navigation.navigate('SwitchResult');
         }}
       />
-    </ScreenWrapper>
+    </Flexbox.Item>
   );
 };
 
