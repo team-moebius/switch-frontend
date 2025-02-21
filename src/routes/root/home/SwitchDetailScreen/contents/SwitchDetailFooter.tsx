2d99054aad76ec4eff54cab4eb20f9d54e04cde3
FerrariRoma <win9592@gmail.com>
2025-02-22 02:41:37 +0900

    [feat] 제안한 스위치 대신 navigation 버튼으로 수정

--- src/routes/root/home/SwitchDetailScreen/contents/SwitchDetailFooter.tsx
+++ src/routes/root/home/SwitchDetailScreen/contents/SwitchDetailFooter.tsx
@@ -18,8 +18,8 @@ interface SwitchDetailFooterProp {
   onPressReport: () => void;
   onPressPropose: () => void;
   onPressRevoke: () => void;
+  onPressCurrentSwitch: () => void;
   userSummaryData: UserSummaryData;
-  offeredList: ItemResponse[];
   isMine: boolean;
 }
 
@@ -28,16 +28,12 @@ const SwitchDetailFooter = ({
   onPressPropose,
   onPressRevoke,
   userSummaryData,
-  offeredList,
+  onPressCurrentSwitch,
   isMine,
 }: SwitchDetailFooterProp) => {
-  const onPressOfferList = (item: any) => {
-    // TODO : 🚨 해당 아이템으로 navigation 걸어야 됨
-    Alert.alert(item, '을 클릭했습니다.');
-  };
   // TODO : 🚨 여기 pairedItemName 처럼 Switches 호출하면 있는 프로퍼티들이 존재한다면,
   // 여기에 걸릴 수 있도록 하기. 일단 Mock data 활용해서 UI 만들기
-  if (true) {
+  if (false) {
     return (
       <Flexbox
         alignItems='center'
@@ -156,28 +152,14 @@ const SwitchDetailFooter = ({
           pr={PADDING.wrapper.horizontal}
           pb={25}
         >
-          <Flexbox justifyContent='center' mb={20}>
-            <Typography
-              children={'이 물품으로 제안한 스위치'}
-              fontSize={FONT_SIZE.header}
-            />
-          </Flexbox>
-          {offeredList.map((offer, index) => {
-            return (
-              <TradingListItem
-                key={index}
-                onPress={() => onPressOfferList(offer.name)}
-                childDirection='column'
-                data={{
-                  src: offer.images ? offer.images[0] : '',
-                  name: offer.name ? offer.name : '',
-                  preferredLocation: offer.preferredLocations
-                    ? Array.from(offer.preferredLocations)[0]
-                    : '',
-                }}
-              />
-            );
-          })}
+          <Button
+            type={'normal'}
+            size={'medium'}
+            onPress={onPressCurrentSwitch}
+          >
+            {/* TODO : props 추가해서 변수로 넣기 */}
+            진행 중인 N건의 스위치 보기
+          </Button>
         </Box>
       );
     }
