611b0a71d9a201151699fdb84c36fffd5926ea11
FerrariRoma <win9592@gmail.com>
2025-02-26 12:48:08 +0900

    [feat] SwitchResultScreen에서 호환되도록 수정

--- src/components/template/WithMirror.tsx
+++ src/components/template/WithMirror.tsx
@@ -33,10 +33,18 @@ const WithMirror = ({
   centerAxis,
 }: WithMirrorProps) => {
   return (
-    <Flexbox {...mirrorDirectionStyle[mirrorDirection]} {...WidthStyle.default}>
-      <Flexbox.Item flex={1}>{renderItem[0]}</Flexbox.Item>
+    <Flexbox
+      {...mirrorDirectionStyle[mirrorDirection]}
+      {...WidthStyle.default}
+      width={'100%'}
+    >
+      <Flexbox.Item flex={1}>
+        <Flexbox justifyContent='center'>{renderItem[0]}</Flexbox>
+      </Flexbox.Item>
       <Flexbox.Item>{centerAxis}</Flexbox.Item>
-      <Flexbox.Item flex={1}>{renderItem[1]}</Flexbox.Item>
+      <Flexbox.Item flex={1}>
+        <Flexbox justifyContent='center'>{renderItem[1]}</Flexbox>
+      </Flexbox.Item>
     </Flexbox>
   );
 };
