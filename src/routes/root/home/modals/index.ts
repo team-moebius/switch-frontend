b45906cd8a71d5bd61896de2c61be7fd6570ad4c
FerrariRoma <win9592@gmail.com>
2025-03-06 22:13:25 +0900

    [feat] SwitchDetail에서 사용하는 modal 위치 수정

--- src/routes/root/home/modals/index.ts
+++ src/routes/root/home/modals/index.ts
@@ -1,5 +1,3 @@
 import { CancelEditModal } from './CancelEditModal';
-import { DeleteItemModal } from './DeleteItemModal';
-import { MyItemOptionModal } from './MyItemOptionModal';
 
-export { CancelEditModal, DeleteItemModal, MyItemOptionModal };
+export { CancelEditModal };
