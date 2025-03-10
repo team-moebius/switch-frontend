d3e0697507ab4bb9770c316274d664a74fa9526f
FerrariRoma <win9592@gmail.com>
2025-03-11 01:42:53 +0900

    [feat] SwitchDetailButton의 태그 통일

--- src/routes/root/home/SwitchDetailScreen/contents/SwitchDetailButton.tsx
+++ src/routes/root/home/SwitchDetailScreen/contents/SwitchDetailButton.tsx
@@ -34,29 +34,26 @@ const SwitchDetailButton = ({
             진행 중인 N건의 스위치 보기
           </Button>
         </Box>
-      ) : (
-        <Flexbox
-          alignItems={'center'}
-          flexDirection={'column'}
-          gap={10}
-          pb={20}
+      ) : isSuggested ? (
+        <Box
           pl={PADDING.wrapper.horizontal}
           pr={PADDING.wrapper.horizontal}
+          pb={25}
         >
-          {isSuggested ? (
-            <Box>
-              <Button type={'warning'} size={'medium'} onPress={onPressRevoke}>
-                요청 취소하기
-              </Button>
-            </Box>
-          ) : (
-            <Box>
-              <Button type={'normal'} size={'medium'} onPress={onPressPropose}>
-                스위치 요청하기
-              </Button>
-            </Box>
-          )}
-        </Flexbox>
+          <Button type={'warning'} size={'medium'} onPress={onPressRevoke}>
+            요청 취소하기
+          </Button>
+        </Box>
+      ) : (
+        <Box
+          pl={PADDING.wrapper.horizontal}
+          pr={PADDING.wrapper.horizontal}
+          pb={25}
+        >
+          <Button type={'normal'} size={'medium'} onPress={onPressPropose}>
+            스위치 요청하기
+          </Button>
+        </Box>
       )}
     </>
   );
